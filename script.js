1f67ecdf42d48b498de9db7728559ad437e2144b
Angel Padilla <angelpadillasanchez@hotmail.com>
2021-11-20 15:51:22 -0600

    Agregando decodificación y codificación

--- script.js
+++ script.js
@@ -8,16 +8,24 @@ function getBase64(file) {
   }
   
 let boton = document.getElementById('decode');
-let p_decoded =  document.getElementById('decoded_string')
+let div_decoded =  document.getElementById('decoded_string')
 let img_decoded = document.getElementById('decoded');
+let decode_button = document.getElementById('decode_button');
 boton.addEventListener('click' , ()=>{
 
     var file = document.querySelector('#imagen').files[0];
     getBase64(file).then(
       data => { 
-        img_decoded.src=data
-        p_decoded.textContent = data
-            }
+        div_decoded.innerHTML = data;
+        div_decoded.data = data
+      }
     );
 });
+
+decode_button.addEventListener('click' , ()=>{
+  let get_data_from_base_64_div = div_decoded.data;
+  console.log(get_data_from_base_64_div)
+  img_decoded.src = div_decoded.data
+})
+
 
